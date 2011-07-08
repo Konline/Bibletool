1c98476c286377d49d44778000141acede39da4c
Jung Ko <kojung@gmail.com>
2011-07-07 22:09:13 -0700

    footnote gated by lack of glossary url

--- js/Browse.js
+++ js/Browse.js
@@ -3,11 +3,39 @@ var Browse = {
   browse: function (url) {
     $('#browse-body').empty();
     var jqxhr = $.getJSON(url, Navigation.getCurrentStyleFn(url))
+      .complete(function(){Browse.glossary(url)})
       .error(function(){
         $('<p>Failed to download data from the server</p>').appendTo('#browse-body');
       });
   },
   
+  // Add glossary foot note once the main body is displayed
+  glossary: function(url) {
+    var url = url.replace('retrieve', 'glossary/retrieve');
+    var jqxhr = $.getJSON(url, function(data) {
+      var footnote = $('<div class=footnote></div>');
+      for (var i=0; i<data.length; i++) {
+        var chinese = data[i].chinese;
+        var english = data[i].english;
+        var book = data[i].book;
+        var chapter = data[i].chapter;
+        var startVerse = data[i].startVerse;
+        var endVerse = data[i].endVerse;
+        var footnoteReference = $('<div class=footnote-reference>' +
+                                  chapter + ':' + book + '</div>');
+        var link = webroot + '/glossary/word/' + chinese;
+        var anchor = $('<a href='+link+'>'+chinese+
+                       (english ? '('+english+')' : "") +
+                       '</a>');
+        var footnoteContent = $('<div class=footnote-content></div>')
+        footnoteReference.appendTo(footnote);
+        anchor.appendTo(footnoteContent);
+        footnoteContent.appendTo(footnote);
+      }
+      footnote.appendTo($('#browse-body'));
+    });
+  },
+
   updateToolbarFn: function(parsedURLFragments) {
     var versions = parsedURLFragments[0];
     var book = parsedURLFragments[1];
