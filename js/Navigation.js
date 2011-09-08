6f4e86765808f5f4edc79a077989d76998cc2afa
Yao Ko <yaoyao@gmail.com>
2011-09-08 03:04:31 -0700

    Fix indentation.

--- js/Navigation.js
+++ js/Navigation.js
@@ -314,10 +314,10 @@ var Navigation = {
         '>Audio version for this chapter</a></div>')
         .appendTo('#browse-body');
       if ( version == 'UCV' ) {
-	// Add acknowledgment, see https://github.com/Konline/Bibletool/issues/93
+        // Add acknowledgment, see https://github.com/Konline/Bibletool/issues/93
         Anarchy.Mp3.go('<div id=browse-chapter-audio-acknowledgment>感謝 <a target="_blank" href="http://www.haomuren.org/">好牧人網站</a> 提供有聲聖經</div>');
       } else {
-	// No need to acknowledge
+        // No need to acknowledge
         Anarchy.Mp3.go();
       }
     }
