246a375a6700b8d59c84091a2750d7ede3c67cd2
Yao Ko <yaoyao@gmail.com>
2014-09-27 17:18:27 -0700

    Find typos by comparing UCV bible text with an external source.

---
+++ typos/convert.py
@@ -0,0 +1,100 @@
+# -*- coding: latin-1 -*-
+
+# Extracts the Bible content from the HTML files embedded in:
+# https://sites.google.com/site/downloadbibles/uniont
+
+import glob
+import re
+
+BOOKS_MAPPING = {
+    1: '創',
+    2: '出',
+    3: '利',
+    4: '民',
+    5: '申',
+    6: '書',
+    7: '士',
+    8: '得',
+    9: '撒上',
+    10: '撒下',
+    11: '王上',
+    12: '王下',
+    13: '代上',
+    14: '代下',
+    15: '拉',
+    16: '尼',
+    17: '斯',
+    18: '伯',
+    19: '詩',
+    20: '箴',
+    21: '傳',
+    22: '歌',
+    23: '賽',
+    24: '耶',
+    25: '哀',
+    26: '結',
+    27: '但',
+    28: '何',
+    29: '珥',
+    30: '摩',
+    31: '俄',
+    32: '拿',
+    33: '彌',
+    34: '鴻',
+    35: '哈',
+    36: '番',
+    37: '該',
+    38: '亞',
+    39: '瑪',
+    40: '太',
+    41: '可',
+    42: '路',
+    43: '約',
+    44: '徒',
+    45: '羅',
+    46: '林前',
+    47: '林後',
+    48: '加',
+    49: '弗',
+    50: '腓',
+    51: '西',
+    52: '帖前',
+    53: '帖後',
+    54: '提前',
+    55: '提後',
+    56: '多',
+    57: '門',
+    58: '來',
+    59: '雅',
+    60: '彼前',
+    61: '彼後',
+    62: '約壹',
+    63: '約貳',
+    64: '約參',
+    65: '猶',
+    66: '啟',
+}
+
+def main():
+    files = glob.glob('*.htm')
+    book = 1
+    for file in files:
+        line_num = 1
+        with open(file) as inf:
+            line = inf.readline()
+            while line:
+                match = re.match('<small>(\d+):(\d+)</small> (.*)<br>', line)
+                if match:
+                    chapter = match.group(1)
+                    verse = match.group(2)
+                    content = match.group(3)
+                    print '%s %s:%s--%s' % (
+                            BOOKS_MAPPING[book], chapter, verse, content)
+
+                line = inf.readline()
+                line_num += 1
+        book += 1
+
+
+if __name__ == '__main__':
+    main()
